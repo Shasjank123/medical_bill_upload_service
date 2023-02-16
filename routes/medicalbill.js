029993498a3cea1d3b18dc249656ef21bfa49bfc
Shashank Balija <bshashank1998@gmail.com>
2023-02-15 20:39:32 -0700

    Add files via upload

---
+++ routes/medicalbill.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const medicalBillsData = data.medicalbills;
+
+
+router
+  .route('/items')
+  .get( async (req, res) => {
+    try {
+      const billsList = medicalBillsData.medicalbills();
+      res.status(200).json(billsList);
+    } catch (e) {
+      // Something went wrong with the server!
+      res.status(500).send(e);
+    }
+  })
+  .post(async (req, res) => {
+    // Not implemented
+    medicalBillsData.addmedicalBill(req.body);
+    res.status(200).json(medicalBillsData.medicalbills());
+  })
+
+module.exports = router;
